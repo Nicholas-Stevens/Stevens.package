782fa68246d936133e9a2ab58a8ce0ef67e75eda
nick stevens <nicholas.stevens@selu.edu>
2020-11-23 12:15:57 -0600

    comitting second function

---
+++ R/Diameter Difference calc.R
@@ -0,0 +1,23 @@
+#' Calculates diameter difference from tree species diameters and finds difference between years
+#' 
+#' @param Diameters_2019_ name of data table used to calculate basil area
+#' @param diameter_2017 yearly diameter measurement for 2017 (numeric, no default)
+#' @param diameter_2018 yearly diameter measurement for 2018 (numeric, no default)
+#' @param diameter_2019 yearly diameter measurement for 2019 (numeric, no default)
+#' @importFrom assertthat assert_that
+#' @return differences between yearly diameters 
+#' 
+#' @export
+#' 
+#' code:
+#' @example
+#' 
+diameter_diff_y2017_2018 <- function(data = diameters_2019,
+                                       diameter_2017 = diameter_2019$diameter_2017,
+                                       diameter_2018 = diameter_2019$diameter_2018)
+   {assertthat::assert_that(is.numeric(data$diameter_2017))
+    assertthat::assert_that(is.numeric(data$diameter_2018))    
+    diameter_diff_calc <- data %>% mutate(diameter_diff_calc = diameter_2018 - diameter_2017)
+    return(diameter_diff_calc)}
+
+diameter_diff_y2017_2018(diameter_filetered, diameter_2017, diameter_2018)
