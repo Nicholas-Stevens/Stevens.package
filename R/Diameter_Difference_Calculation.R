319f478fe919a9bab4414e87ec0f7a36e489e14d
nick stevens <nicholas.stevens@selu.edu>
2020-12-02 23:22:28 -0600

    comitting

--- R/Diameter_Difference_Calculation.R
+++ R/Diameter_Difference_Calculation.R
@@ -1,19 +1,22 @@
 #' Calculates diameter difference from tree species diameters and finds difference between years
 #' 
-#' @param Diameters_2019_ name of data table used to calculate basil area
+#' @param Diameters name of data table used to calculate diameter difference
 #' @param diameter_2017 yearly diameter measurement for 2017 (numeric, no default)
 #' @param diameter_2018 yearly diameter measurement for 2018 (numeric, no default)
-#' @param diameter_2019 yearly diameter measurement for 2019 (numeric, no default)
 #' @importFrom assertthat assert_that
+#' @importFrom magrittr %>% 
+#' @importFrom dplyr mutate
+#' 
 #' @return differences between yearly diameters (data frame)
 #' 
 #' @export
 
+
 diameter_diff_y2017_2018 <- function(data = Diameters,
                                        diameter_2017 = Diameters$diameter_2017,
-                                       diameter_2018 = Diameters$diameter_2018)
+                                       diameter_2018 = Diameters$diameter_2018){
    {assertthat::assert_that(is.numeric(data$diameter_2017))
     assertthat::assert_that(is.numeric(data$diameter_2018))    
     diameter_diff_calc <- data %>% mutate(diameter_diff_calc = diameter_2018 - diameter_2017)
     return(diameter_diff_calc)}
-
+}
--- R/Leaf_Litter_Calculation.R
+++ R/Leaf_Litter_Calculation.R
@@ -3,21 +3,20 @@
 #' @param Leaf_litter_filtered name of data table (tibble, no default)
 #' @param Y2017 year 2017 leaf litter weights (numeric, no default)
 #' @param Y2018 year 2018 leaf litter weights (numeric, no default)
-#' @param Y2019 year 2019 leaf litter weights (numeric, no default)
 #' @return difference between yearly leaf litter weights (numeric)
 #' @importFrom assertthat assert_that
+#' @importFrom magrittr %>% 
+#' @importFrom dplyr mutate
 #' 
 #' @export
 #'
 
-
-
 leaf_diff_y2017_2018 <- function(data = Leaf_litter_filtered,
                                 Y2017 = Leaf_litter_filtered$Y2017,
-                                Y2018 = Leaf_litter_filtered$Y2018)
+                                Y2018 = Leaf_litter_filtered$Y2018){
   {assertthat::assert_that(is.numeric(data$Y2017))
    assertthat::assert_that(is.numeric(data$Y2018))    
    leaf_diff_calc <- data %>% mutate(leaf_diff_calc = Y2018 - Y2017)
    return(leaf_diff_calc)}
-
+}
 
